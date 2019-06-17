434e6ae9dde48b0bd9e0d623100a42ffd9d30367
redcozmo <redcozmo@hotmail.com>
2019-06-17 15:36:58 +0200

    add methods for parsing datas

--- app/models/citybike.rb
+++ app/models/citybike.rb
@@ -2,5 +2,21 @@
 
 class Citybike < ApplicationRecord
   include HTTParty
-  base_uri 'http://api.citybik.es/v2/'
+  base_uri "api.citybik.es"
+
+  def data
+    self.class.get("/v2/networks")
+  end
+
+  def data_velib
+    self.class.get("/v2/networks/velib")
+  end
+
+  def networks
+    data.parsed_response
+  end
+
+  def stations
+    data_velib.parsed_response
+  end
 end
