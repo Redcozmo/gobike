1e3bfafb16d8313c717225cbe8b6cbc7ba705e18
redcozmo <redcozmo@hotmail.com>
2019-06-18 17:28:58 +0200

    add service for calculation of closest stations

---
+++ app/services/closest_stations.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class ClosestStations
+  #
+  # Description :
+  # => Use the ClosestStations class to calculate the closest stations
+  #   to a position with two lon/lat coordnate pairs.
+  #
+  # Output :
+  # => array with <num> closest stations from input coordinate point
+  #
+  # Example usage :
+  # => ClosestStations.new(coord, num).closest_stations
+
+  attr_accessor :longitude, :latitude, :num
+
+  def initialize(coord, num)
+    @longitude, @latitude = coord
+    @num = num
+  end
+
+  def closest_stations
+    @stations = Station.all
+    distance = {}
+    selection = []
+    @stations.map { |station| distance[station.station_id] = Haversine.new([station.longitude, station.latitude], [@longitude, @latitude]).distance }
+    distance.values.min(num).each { |e| selection << distance.key(e) }
+    @stations.select { |station| selection.find { |id| id == station.station_id } }
+  end
+end
