75dbc628e3b0951e189fde156f5a7ec568205da2
redcozmo <redcozmo@hotmail.com>
2019-10-07 17:19:19 +0200

    add map with markers for closest stations

---
+++ app/assets/javascripts/leaflets_icons.js
@@ -0,0 +1,17 @@
+// var LeafIcon = L.Icon.extend({
+// 	options: {
+// 		shadowUrl: '<%= image_url("marker_black.png") %>',
+// 		iconSize:     [38, 95],
+// 		iconAnchor:   [22, 94],
+// 		popupAnchor:  [-3, -76]
+// 	}
+// });
+//
+// var greenIcon = new LeafIcon({iconUrl: 'marker_green.png'}),
+//     redIcon = new LeafIcon({iconUrl: 'marker_red.png'}),
+//     blackIcon = new LeafIcon({iconUrl: 'marker_black.png'});
+//     homeicon =  new LeafIcon({iconUrl: 'icon_home.png'});
+
+// L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map).bindPopup("I am a green leaf.");
+// L.marker([51.495, -0.083], {icon: redIcon}).addTo(map).bindPopup("I am a red leaf.");
+// L.marker([51.49, -0.1], {icon: orangeIcon}).addTo(map).bindPopup("I am an orange leaf.");
